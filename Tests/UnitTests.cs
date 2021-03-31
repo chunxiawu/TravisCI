5f4879a7c49f24ef62617f0bcccd0256bd5d9acc
Chelsea Wu <cwu9@unl.edu>
2021-03-31 12:41:28 -0500

    one test failed

--- Tests/UnitTests.cs
+++ Tests/UnitTests.cs
@@ -10,7 +10,7 @@ namespace TravisCILab
         public void Add_Valid()
         {
             Assert.AreEqual(3, Program.Add("1", "2"));
-            Assert.AreEqual(5, Program.Add("3", "2"));
+            Assert.AreEqual(5, Program.Add("3", "3"));
             Assert.AreEqual(12, Program.Add("5", "7"));
         }
 
@@ -31,5 +31,103 @@ namespace TravisCILab
         }
 
         // Implement 3 tests per operation, following a similar pattern as above
+        [Test]
+        public void Subtract_ValidWu()
+        {
+            Assert.AreEqual(-1, Program.Subtract("1", "2"));
+            Assert.AreEqual(1, Program.Subtract("3", "2"));
+            Assert.AreEqual(-2, Program.Subtract("5", "7"));
+        }
+
+        [Test]
+        public void Subtract_InvalidWu()
+        {
+            Assert.Throws<FormatException>(() => Program.Subtract("1", "a"));
+            Assert.Throws<FormatException>(() => Program.Subtract("a", "1"));
+            Assert.Throws<FormatException>(() => Program.Subtract("a", "a"));
+        }
+
+        [Test]
+        public void Subtract_NullWu()
+        {
+            Assert.Throws<ArgumentNullException>(() => Program.Subtract("1", null));
+            Assert.Throws<ArgumentNullException>(() => Program.Subtract(null, "1"));
+            Assert.Throws<ArgumentNullException>(() => Program.Subtract(null, null));
+        }
+        // Implement 3 tests per operation, following a similar pattern as above
+        [Test]
+        public void Multiply_ValidWu()
+        {
+            Assert.AreEqual(2, Program.Multiply("1", "2"));
+            Assert.AreEqual(6, Program.Multiply("3", "2"));
+            Assert.AreEqual(35, Program.Multiply("5", "7"));
+        }
+
+        [Test]
+        public void Multiply_InvalidWu()
+        {
+            Assert.Throws<FormatException>(() => Program.Multiply("1", "a"));
+            Assert.Throws<FormatException>(() => Program.Multiply("a", "1"));
+            Assert.Throws<FormatException>(() => Program.Multiply("a", "a"));
+        }
+
+        [Test]
+        public void Multiply_NullWu()
+        {
+            Assert.Throws<ArgumentNullException>(() => Program.Multiply("1", null));
+            Assert.Throws<ArgumentNullException>(() => Program.Multiply(null, "1"));
+            Assert.Throws<ArgumentNullException>(() => Program.Multiply(null, null));
+        }
+
+        // Implement 3 tests per operation, following a similar pattern as above
+        [Test]
+        public void Divide_ValidWu()
+        {
+            Assert.AreEqual(0.5, Program.Divide("1", "2"));
+            Assert.AreEqual(1.5, Program.Divide("3", "2"));
+            Assert.AreEqual(1.25, Program.Divide("5", "4"));
+        }
+
+        [Test]
+        public void Divide_InvalidWu()
+        {
+            Assert.Throws<FormatException>(() => Program.Divide("1", "a"));
+            Assert.Throws<FormatException>(() => Program.Divide("a", "1"));
+            Assert.Throws<FormatException>(() => Program.Divide("a", "a"));
+        }
+
+        [Test]
+        public void Divide_NullWu()
+        {
+            Assert.Throws<ArgumentNullException>(() => Program.Divide("1", null));
+            Assert.Throws<ArgumentNullException>(() => Program.Divide(null, "1"));
+            Assert.Throws<ArgumentNullException>(() => Program.Divide(null, null));
+        }
+
+        // Implement 3 tests per operation, following a similar pattern as above
+        [Test]
+        public void Power_ValidWu()
+        {
+            Assert.AreEqual(1, Program.Power("1", "2"));
+            Assert.AreEqual(9, Program.Power("3", "2"));
+            Assert.AreEqual(78125, Program.Power("5", "7"));
+        }
+
+        [Test]
+        public void Power_InvalidWu()
+        {
+            Assert.Throws<FormatException>(() => Program.Power("1", "a"));
+            Assert.Throws<FormatException>(() => Program.Power("a", "1"));
+            Assert.Throws<FormatException>(() => Program.Power("a", "a"));
+        }
+
+        [Test]
+        public void Power_NullWu()
+        {
+            Assert.Throws<ArgumentNullException>(() => Program.Power("1", null));
+            Assert.Throws<ArgumentNullException>(() => Program.Power(null, "1"));
+            Assert.Throws<ArgumentNullException>(() => Program.Power(null, null));
+        }
+
     }
 }
